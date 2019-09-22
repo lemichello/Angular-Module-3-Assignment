1f36531450e4f8257ad98f123c965082cf38e0c7
Maksym Lemich <lemichello.m@gmail.com>
2019-09-22 18:53:58 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+(function () {
+    'use-strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItems);
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        let ctrl = this;
+
+        ctrl.input = "";
+
+        ctrl.clicked = function () {
+            let promise = MenuSearchService.getMatchedMenuItems(ctrl.input);
+
+            promise.then(function (result) {
+                ctrl.found = result;
+            });
+        };
+
+        ctrl.removeItem = function (index) {
+            ctrl.found.splice(index, 1);
+        };
+    }
+
+    MenuSearchService.$inject = ['$http'];
+
+    function MenuSearchService($http) {
+        let service = this;
+
+        service.getMatchedMenuItems = function (searchTerm) {
+            return $http({
+                method: "GET",
+                url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
+            }).then(function (response) {
+                let foundItems = response.data.menu_items;
+                let filteredItems = [];
+
+                for (let item of foundItems) {
+                    if (item.description === searchTerm) {
+                        filteredItems.push(item)
+                    }
+                }
+
+                return filteredItems;
+            })
+        }
+    }
+
+    function FoundItems() {
+        return {
+            restrict: 'E',
+            templateUrl: 'loader/itemsloaderindicator.template.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            },
+            controller: NarrowItDownController,
+            controllerAs: 'itemCtrl',
+            bindToController: true
+        };
+    }
+})();
